c3db702e886a830dae8a92cb3189bfe4030a2ffd
LEONARDO ROBERTO AMADIO <22.01300-8@maua.br>
2025-05-10 12:56:19 -0300

    learn(07-react): encerra a apostila - retomar fazendo o back end entre front e API Pexels a fim de isolar a chave

--- exibe-fotos/src/App.jsx
+++ exibe-fotos/src/App.jsx
@@ -1,14 +1,59 @@
+import env from 'react-dotenv'
+import { createClient } from 'pexels'
 import Busca from "./components/Busca"
+import React from 'react'
+import pexelsClient from './utils/pexelsClient'
+import ListaImagens from './components/ListaImagens'
+import PexelsLogo from './components/PexelsLogo'
+export default class App extends React.Component {
 
-const App = () => {
-  return (
-    <div className="grid border-1 justify-content-center w-9 m-auto border-200">
-      <div className="col-12">
-        <h4>Exibir uma lista de...</h4>
+  //pexelsClient = null
+
+  state = {
+    photos: []
+  }
+
+  onBuscaRealizada = (termo) => {
+    pexelsClient.get('/search', {
+      params: {
+        query:termo
+      }
+    })
+    .then(result => this.setState({photos: result.data.photos}))
+  }
+
+  // onBuscaRealizada = (termo) => {
+  //   this.pexelsClient.photos.search({
+  //     query:termo
+  //   })
+  //   .then(result => this.setState({photos: result.photos}))
+  // }
+
+  // componentDidMount(){
+  //   console.log(env.PEXELS_KEY)
+  //   this.pexelsClient = createClient(env.PEXELS_KEY)
+  // }
+
+  render(){
+    return (
+      <div className="grid border-1 justify-content-center w-9 m-auto border-200">
+        <div className='col-12 text-center'>
+          <PexelsLogo/>
+        </div>
+        <div className="col-12 text-center">
+          <h4>Exibir uma lista de...</h4>
+        </div>
+        <div className="col-12">
+          <Busca onBuscaRealizada={this.onBuscaRealizada}/>
+        </div>
+        <div className='col-12'>
+          <div className='grid'>
+            <ListaImagens imgStyle={'md:col-6 lg:col-4 xl: col-3'} photos={this.state.photos}/>
+          </div>
+              
+          
+        </div>
       </div>
-      <Busca />
-    </div>
-  )
+    )
+  }
 }
-
-export default App
--- exibe-fotos/src/components/Busca.jsx
+++ exibe-fotos/src/components/Busca.jsx
@@ -1,29 +1,47 @@
 import React, { Component } from 'react'
-import {Button} from 'primereact/button'
-import {IconField} from 'primereact/iconfield'
-import {InputIcon} from 'primereact/inputicon'
-import {InputText} from 'primereact/inputtext'
+import { Button } from 'primereact/button'
+import { IconField } from 'primereact/iconfield'
+import { InputIcon } from 'primereact/inputicon'
+import { InputText } from 'primereact/inputtext'
 
 
 export default class Busca extends Component {
 
-state = {
+  state = {
     termoDeBusca: ''
-}
-    render() {
+  }
+
+  onTermoAlterado = (event) => {
+    //console.log(event.target.value)
+    this.setState({ termoDeBusca: event.target.value })
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault()
+    this.props.onBuscaRealizada(this.state.termoDeBusca)
+  }
+
+  render() {
     return (
-      <div className='flex flex-column'>
-        <IconField 
+      <form onSubmit={this.onFormSubmit} >
+        <div className='flex flex-column'>
+          <IconField
             iconPosition='left'>
-            <InputIcon className='pi pi-search'/>
-            <InputText className='w-full' placeholder={this.props.dica}/>
-        </IconField>
-        <Button className='mt-1' label='OK' outlined/>
-      </div>
+            <InputIcon className='pi pi-search' />
+            <InputText
+              onChange={this.onTermoAlterado}
+              value={this.state.termoDeBusca}
+              className='w-full'
+              placeholder={this.props.dica} />
+          </IconField>
+          <Button className='mt-1' label='OK' outlined />
+        </div>
+      </form>
+
     )
   }
 }
 
 Busca.defaultProps = {
-    dica: "Buscar..."
+  dica: "Buscar..."
 }
